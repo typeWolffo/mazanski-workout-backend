e59bcdfc413bc0289ebea66524ac47a01a731f6c
Jakub Wilk <jwilk436@gmail.com>
2022-09-09 16:38:20 +0200

    FIX typo

--- src/database/migrations/1662656962603-test.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class test1662656962603 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
